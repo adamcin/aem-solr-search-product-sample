78973fe1ed1f8a6369fd12c62343c85c7a38816a
Mark Adamcin <adamcin@gmail.com>
2016-09-15 17:40:16 -0700

    Upgraded to Solr 6.2.0; massaged vagrant scripts to work in latest vagrant+vbox

--- demo-stack-vagrant/scripts/bootstrap.sh
+++ demo-stack-vagrant/scripts/bootstrap.sh
@@ -2,17 +2,17 @@
 #
 # Description: Provisioning script for my CIRCUIT talk. Creates a CentOS VM with ActiveMQ and Solr.
 
-export INSTALL_HOME=/home/vagrant
-export INSTALL_SCRIPTS=$INSTALL_HOME/sync/scripts
-
-SAVE_CWD=`pwd`
+export INSTALL_HOME="/home/vagrant"
+export INSTALL_SYNC="/vagrant"
+export INSTALL_SCRIPTS="${INSTALL_SYNC}/scripts"
 
 echo "Installing OS packages..."
 yum -y install curl lsof net-tools java-1.8.0-openjdk-devel 
 
-cd $INSTALL_SCRIPTS
+pushd "$INSTALL_SCRIPTS"
 
 ./install-solr.sh
 ./install-activemq.sh
 
-cd $SAVE_CWD
+#unnecessary because end of script execution causes popd to origin dir
+popd
--- demo-stack-vagrant/scripts/install-solr.sh
+++ demo-stack-vagrant/scripts/install-solr.sh
@@ -2,42 +2,41 @@
 #
 # Description: Installs and configures Solr 5.4.1 from scratch.
 
-SOLR_VERSION=5.4.1
-SOLR_TGZ_URL=https://archive.apache.org/dist/lucene/solr/$SOLR_VERSION/solr-$SOLR_VERSION.tgz
+#SOLR_VERSION=5.4.1
+SOLR_VERSION=6.2.0
+SOLR_TGZ_URL="https://archive.apache.org/dist/lucene/solr/${SOLR_VERSION}/solr-${SOLR_VERSION}.tgz"
 SOLR_HOST_NODE1="solrnode1"
 SOLR_HOST_NODE2="solrnode2"
 SOLR_HOME=/opt/solr-home
 SOLR_INSTALL_DIR=/opt/solr
 
-CWD=`pwd`
-
 echo "Downloading Solr ${SOLR_VERSION}..."
-curl -s -O "$SOLR_TGZ_URL"
+curl -s -O "${SOLR_TGZ_URL}"
 
 echo "Updating /etc/hosts..."
-echo "127.0.0.1 $SOLR_HOST_NODE1" >> /etc/hosts
-echo "127.0.0.1 $SOLR_HOST_NODE2" >> /etc/hosts
+echo "127.0.0.1 ${SOLR_HOST_NODE1}" >> /etc/hosts
+echo "127.0.0.1 ${SOLR_HOST_NODE2}" >> /etc/hosts
 
-echo "Installing Solr $SOLR_VERSION..."
-cd /opt
-tar -xzf $INSTALL_SCRIPTS/solr-$SOLR_VERSION.tgz
-ln -s /opt/solr-$SOLR_VERSION $SOLR_INSTALL_DIR
-cp -r /home/vagrant/sync/solr-home /opt
+echo "Installing Solr ${SOLR_VERSION}..."
+pushd /opt
+tar -xzf "${INSTALL_SCRIPTS}/solr-${SOLR_VERSION}.tgz"
+ln -nFfs "/opt/solr-${SOLR_VERSION}" "${SOLR_INSTALL_DIR}"
+rsync -a "${INSTALL_SYNC}/solr-home/" /opt/solr-home/
 
-cd $SOLR_INSTALL_DIR
+pushd "${SOLR_INSTALL_DIR}"
 
 echo "Creating Solr user and fixing permissions..."
-useradd solr
-chown -R solr:solr $SOLR_HOME $SOLR_INSTALL_DIR /opt/solr-$SOLR_VERSION
+id -u solr &>/dev/null || useradd solr
+chown -R solr:solr "${SOLR_HOME}" "${SOLR_INSTALL_DIR}" "/opt/solr-${SOLR_VERSION}"
 
 # Install System V scripts
-$INSTALL_SCRIPTS/install-solr-services.sh
+"${INSTALL_SCRIPTS}/install-solr-services.sh"
 
 echo "Starting Solr node 1..."
 service solr1 start
 
 echo "Uploading movies config set to Zookeeper"
-$SOLR_INSTALL_DIR/server/scripts/cloud-scripts/zkcli.sh -zkhost 127.0.0.1:9983 -cmd upconfig -confname movies -confdir $SOLR_HOME/configsets/movies/conf
+"${SOLR_INSTALL_DIR}/server/scripts/cloud-scripts/zkcli.sh" -zkhost 127.0.0.1:9983 -cmd upconfig -confname movies -confdir "${SOLR_HOME}/configsets/movies/conf"
 
 echo "Starting Solr node 2..."
 service solr2 start
